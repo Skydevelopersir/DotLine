53346347671817b86590c60193854a760aa6888f
Mohammad Javad Bahiraei <programmer.bahiraei@gmail.com>
2018-11-05 20:00:30 +0330

    Update G Class

--- app/src/main/java/project/G.java
+++ app/src/main/java/project/G.java
@@ -3,6 +3,8 @@ package project;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 import android.util.Log;
 
 import ir.skydevelopers.app.project.R;
@@ -12,12 +14,16 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 public class G extends Application{
 
     @SuppressLint("StaticFieldLeak")
-    public static  Context context;
+    public static Context context;
+    public static Resources resources;
+    public static DisplayMetrics displayMetrics;
 
     @Override
     public void onCreate() {
         super.onCreate();
         context=getApplicationContext();
+        resources = context.getResources();
+        displayMetrics = resources.getDisplayMetrics();
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/IRANSans.ttf")
                 .setFontAttrId(R.attr.fontPath)
