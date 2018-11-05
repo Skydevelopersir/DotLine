9b9823318d6d748ebe0557afa2363fc4fc036f48
Mohammad Javad Bahiraei <programmer.bahiraei@gmail.com>
2018-11-05 20:05:14 +0330

    Main Activity Updated

--- app/src/main/java/project/MainActivity.java
+++ app/src/main/java/project/MainActivity.java
@@ -3,6 +3,9 @@ package project;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import ir.skydevelopers.app.project.R;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -13,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnReset = (Button) findViewById(R.id.btn_reset);
+        final GameView gameView = (GameView) findViewById(R.id.gameview);
+
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //gameView.resetGame();
+                Toast.makeText(MainActivity.this, "Game Reset", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
