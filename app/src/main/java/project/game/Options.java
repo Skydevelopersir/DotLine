3699bad7727268c7636fab5a5f3092a06e50d048
Mohammad Javad Bahiraei <programmer.bahiraei@gmail.com>
2018-11-07 11:25:21 +0330

    Updated Some Codes

--- app/src/main/java/project/game/Options.java
+++ app/src/main/java/project/game/Options.java
@@ -11,6 +11,6 @@ public class Options {
     public static int rows = 6;
 
     public static String[] playerNames = new String[]{G.resources.getString(R.string.player_1), G.resources.getString(R.string.player_2)};
-    public static int[] playerTypes = new int[]{TYPE_CPU, TYPE_CPU};
+    public static int[] playerTypes = new int[]{TYPE_CPU, TYPE_PLAYER};
     //private static int[] playerTypes = new int[]{TYPE_PLAYER, TYPE_PLAYER};
 }
